02bea33c3e1f61711632bbae9f27b115bd1b273e
Sasha <lonagi22@gmail.com>
2020-07-04 00:14:49 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 from setuptools import setup, find_packages
 from os.path import join, dirname
 
-import pynvg
-
 __PACKAGE__='pynvg'
 __DESCRIPTION__='pynvg is a general purpose library by NVG'
-__VERSION__=pynvg.__version__
+__VERSION__="0.0.4"
 
 setup(
     name=__PACKAGE__,
     version=__VERSION__,
-    packages=find_packages(include=__PACKAGE__),
+    packages=['pynvg'],
     description=__DESCRIPTION__,
     long_description=open(join(dirname(__file__), 'README.rst')).read(),
     author="lonagi",
