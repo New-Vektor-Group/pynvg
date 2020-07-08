5500e0ea7f5777c1c70c1c19d58d35f6936c0ef4
Sasha <lonagi22@gmail.com>
2020-07-09 00:06:14 +0300

    Update test_ordlist.py

--- test/text/test_ordlist.py
+++ test/text/test_ordlist.py
@@ -7,3 +7,5 @@ class OrdListTestCase(unittest.TestCase):
         expectedList=[72,101,108,108,111]
         self.assertEqual(ordlist("Hello"),expectedList)
 
+if(__name__=="main"):
+    unittest.main()
