487f5dd349ab1b64ed0803a4aced2bf3cb69fafa
Sasha <lonagi22@gmail.com>
2020-07-09 00:06:12 +0300

    Create test_ordlist.py

---
+++ test/text/test_ordlist.py
@@ -0,0 +1,9 @@
+import unittest
+from pynvg.text import ordlist
+
+class OrdListTestCase(unittest.TestCase):
+
+    def test_ordlist(self):
+        expectedList=[72,101,108,108,111]
+        self.assertEqual(ordlist("Hello"),expectedList)
+
