cc22e6935955e22d8d577dcf1eac03c08c45b1a1
Sasha <lonagi22@gmail.com>
2020-07-09 00:06:32 +0300

    Update test_isset.py

--- test/var/test_isset.py
+++ test/var/test_isset.py
@@ -6,5 +6,9 @@ class issetTestCase(unittest.TestCase):
     def test_isset(self):
        	a=173
         self.assertTrue(isset(a))
+
+    def test_isntset(self):
+        self.assertFalse(isset(b))
+
 if(__name__=="main"):
     unittest.main()
