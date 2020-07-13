8481a1dcd0bd47ea9bd1ae822cb69b7542dc0c5d
Sasha <lonagi22@gmail.com>
2020-07-14 00:13:06 +0300

    another tests for abool func

--- test/var/test_abool.py
+++ test/var/test_abool.py
@@ -6,8 +6,22 @@ class ABoolTestCase(unittest.TestCase):
     def test_aboolTrue(self):
         result=abool(True)
         self.assertTrue(result)
+
     def test_aboolFalse(self):
         result=abool(False)
         self.assertFalse(result)
+
+    def test_aboolOne(self):
+        result=abool(1)
+        self.assertTrue(result)
+
+   	def test_aboolTextTrue(self):
+        result=abool("Hello World")
+        self.assertTrue(result)
+
+    def test_aboolTextFalse(self):
+        result=abool("")
+        self.assertFalse(result)
+
 if(__name__=="main"):
     unittest.main()
