a5ab10daa46748992901a6b426c25eb870193a29
Sasha <lonagi22@gmail.com>
2020-07-14 00:12:54 +0300

    test_aboolFalse

--- test/var/test_abool.py
+++ test/var/test_abool.py
@@ -6,5 +6,8 @@ class ABoolTestCase(unittest.TestCase):
     def test_aboolTrue(self):
         result=abool(True)
         self.assertTrue(result)
+    def test_aboolFalse(self):
+        result=abool(False)
+        self.assertFalse(result)
 if(__name__=="main"):
     unittest.main()
