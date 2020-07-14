efe2da4bc1117232e60e0c0ee499fcf0ce3d4322
Sasha <lonagi22@gmail.com>
2020-07-15 00:39:46 +0300

    bitvector Index orientation

--- test/var/test_bitvector.py
+++ test/var/test_bitvector.py
@@ -5,17 +5,17 @@ class BitVectorTestCase(unittest.TestCase):
 
     def test_bitvector2(self):
         expected=bitvector(1)
-        result=[1,0]
+        result=[0,1]
         self.assertEqual(result,expected)
 
     def test_bitvector5(self):
         expected=bitvector(2,5)
-        result=[0,1,0,0,0]
+        result=[0,0,1,0,0]
         self.assertEqual(result,expected)
 
     def test_bitvectorTrue(self):
         expected=bitvector(True)
-        result=[1,0]
+        result=[0,1]
         self.assertEqual(result,expected)
 
 if(__name__=="main"):
