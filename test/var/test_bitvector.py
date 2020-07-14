cd9f2c8c2284520308324d229e7a555e9046ea3f
Sasha <lonagi22@gmail.com>
2020-07-15 00:27:14 +0300

    test bitvector

---
+++ test/var/test_bitvector.py
@@ -0,0 +1,11 @@
+import unittest
+from pynvg.var import bitvector
+
+class BitVectorTestCase(unittest.TestCase):
+
+    def test_bitvector2(self):
+        expected=bitvector(1)
+        result=[1,0]
+        self.assertEqual(result,expected)
+if(__name__=="main"):
+    unittest.main()
