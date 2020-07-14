5f538981502a3ea02f60cd4f96c9b6097c96697e
Sasha <lonagi22@gmail.com>
2020-07-15 00:28:08 +0300

    0.0.6 version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 __PACKAGE__='pynvg'
 __DESCRIPTION__='pynvg is a general purpose library by NVG'
-__VERSION__="0.0.5"
+__VERSION__="0.0.6"
 
 setup(
     name=__PACKAGE__,
