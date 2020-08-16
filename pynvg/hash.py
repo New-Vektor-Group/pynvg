76fd42b86bd83962240a2cff790a716c5a5d01d8
Sasha <lonagi22@gmail.com>
2020-08-16 23:06:09 +0300

    transform password to hashed

--- pynvg/hash.py
+++ pynvg/hash.py
@@ -4,3 +4,13 @@ from Crypto.Hash import SHA256
 from Crypto.Hash import MD5
 from Crypto.PublicKey import RSA
 from Crypto import Random
+def transform_password(password_str):
+    """Transform the password string into 32 bit MD5 hash
+        
+    :param password_str: <str> password in plain text;
+    :return: <str> Transformed password fixed length
+            
+    """
+    h = MD5.new()
+    h.update(password_str.encode())
+    return h.hexdigest()
