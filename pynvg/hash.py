d81c7095be28a55f60ae1d5f0942720d2941da73
Sasha <lonagi22@gmail.com>
2020-08-16 23:06:24 +0300

    symmetric encryption

--- pynvg/hash.py
+++ pynvg/hash.py
@@ -14,3 +14,21 @@ def transform_password(password_str):
     h = MD5.new()
     h.update(password_str.encode())
     return h.hexdigest()
+def symmetric_encrypt(message, key, verbose = False):
+    """Encripts the message using symmetric AES algorythm.
+        
+    :param message: <str> Message for encryption;
+    :param key: <object> symmetric key;
+    :return: <object> Message encrypted with key
+            
+    """
+
+    key_MD5 = transform_password(key)
+    message_hash = SHA256.new(message.encode())
+    message_with_hash = message.encode() + message_hash.hexdigest().encode() 
+    iv = Random.new().read(AES.block_size)
+    cipher = AES.new(key_MD5, AES.MODE_CFB, iv)
+    encrypted_message = iv + cipher.encrypt(message_with_hash)
+    if verbose:
+        print(f'Message was encrypted into: {encrypted_message.hex()}')
+    return encrypted_message
